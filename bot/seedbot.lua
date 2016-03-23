7d5c4fa3cf9143d21bf8d897e9f4a8cf96e12403
kia2877 <kiakako25.4@gmail.com>
2016-03-23 19:54:03 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {108402524},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -251,8 +251,7 @@ Our channels:
 English: @TeleSeedCH
 Persian: @IranSeed
 ]],
-    help_text_realm = [[
-Realm Commands:
+    help_text_realm = [Realm Commands:
 
 !creategroup [name]
 Create a group
